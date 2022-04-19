df0645813ef9262d2134fcffb4cb1b292a65c416
Adrián Bíro <biroadrian@protonmail.com>
2022-04-19 18:27:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+A try for the organization of a personal list of books.
+
+
+* TODO Clean-up of data to SQLite format for comfortable querying
+
+* TODO sort names
---
+++ zorad_knihy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import csv
+"""A try for the organization of a personal list of books."""
+# TODO Clean-up of data to SQLite format for comfortable querying
+# TODO sort names
+
+with open("Archiv_literatura.csv", 'w', newline='') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',dialect='unix', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(["Name", "Title", "Town", "Publisher", "Date"])
+    with open("Archiv-literatúra.txt") as f:
+        for i in f.readlines():
+            i.replace("• ", "")
+            name, *title = i.replace("•", "").strip().split(":")
+            name = name.strip()  # name
+            title = " ".join(title)
+            title, *pub_date = title.split(",")
+            *title, town = title.split(".")
+            if not town:
+                town = "None"
+            town = town.strip()  # town
+            title = " ".join(title)  # title
+            try:
+                publisher, *date = pub_date
+            except ValueError:
+                publisher, date  = title, "None"
+            if not town:
+                publisher = "None"
+            publisher = publisher.strip()  # publisher
+            date = " ".join(date).strip()
+            if date == "N o n e":
+                date = "None"  # date
+            filewriter.writerow([name, title, town, publisher, date])
